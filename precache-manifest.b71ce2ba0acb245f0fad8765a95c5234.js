fd35279570ab890f3d9e3334f664517143ad26f8
shaaandi <saif.awan.10441@gmail.com>
2020-06-28 14:11:18 +0500

    Updates

---
+++ precache-manifest.b71ce2ba0acb245f0fad8765a95c5234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbb8073398863910905fb85c8b045cc",
+    "url": "/ar-study-frontend-app/index.html"
+  },
+  {
+    "revision": "5f4fc908e1afd94f5d73",
+    "url": "/ar-study-frontend-app/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "7092777c228b892fab3c",
+    "url": "/ar-study-frontend-app/static/css/main.c46c607a.chunk.css"
+  },
+  {
+    "revision": "5f4fc908e1afd94f5d73",
+    "url": "/ar-study-frontend-app/static/js/2.c27ee629.chunk.js"
+  },
+  {
+    "revision": "8a964cf507c7e649be90fda3c42d2a45",
+    "url": "/ar-study-frontend-app/static/js/2.c27ee629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7092777c228b892fab3c",
+    "url": "/ar-study-frontend-app/static/js/main.d0a52a49.chunk.js"
+  },
+  {
+    "revision": "5ed4357f1b38b10224ff",
+    "url": "/ar-study-frontend-app/static/js/runtime-main.c0f71aac.js"
+  }
+]);
